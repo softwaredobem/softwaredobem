e38323ac829a425efbcb60504717beb795a30878
Tulio Vitor Machado Faria <tuliofaria@gmail.com>
2020-10-02 16:33:12 -0300

    Adding tailwind. Closes #1

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import '../css/styles.css'
+
+const App = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />
+}
+export default App
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Head from 'next/head'
 
 const Index = () => {
   return (
-    <div>
-      <h1>Sofware do Bem</h1>
+    <div className="h-full w-full text-center mt-12">
+      <Head>
+        <title>Software do Bem</title>
+      </Head>
+      <h1 className="text-4xl font-bold shadow-lg rounded-md p-12 inline-block">
+        Sofware do Bem
+      </h1>
     </div>
   )
 }
